fe26ef0ef730a92221033671511cfb3bba39b610
Leorouge <luckysbam7@gmail.com>
2025-03-05 00:25:44 -0700

    boots helped main

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-def get_book_text():
-    with open("books/frankenstein.txt") as f:
+def get_book_text(path):
+    with open(path) as f:
         file_contents = f.read()
+    return file_contents
 
 def main():
-    get_book_text
-    print (file_contents)
+    text = get_book_text("books/frankenstein.txt")
+    print (text)
 
 main()
 
