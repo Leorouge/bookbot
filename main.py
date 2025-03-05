82a44bd944b759de7b9c6474e7d4173882709ce5
Leorouge <luckysbam7@gmail.com>
2025-03-04 23:57:30 -0700

    main edit boot file content

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("greetings boots")
+def get_book_text:
+    with open(books/frankenstein.txt) as f:
+        file_contents = f.read()
+
+def main():
+    call get_book_text
+    print (file_contents)
+
+main()
