d103691ece0c30d38ccb3bdbffef200a42bc634c
Leorouge <luckysbam7@gmail.com>
2025-03-05 00:12:04 -0700

    edit main

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 def get_book_text():
-    with open(books/frankenstein.txt) as f:
+    with open("books/frankenstein.txt") as f:
         file_contents = f.read()
 
 def main():
-    call get_book_text
+    get_book_text
     print (file_contents)
 
 main()
